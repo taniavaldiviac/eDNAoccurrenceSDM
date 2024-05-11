d289cfb1ebff83f68f4e18ffb4120d7fa4d587ea
Tania Valdivia Carrillo <tania.valdiviac@gmail.com>
2024-05-11 09:01:22 -0700

    Simplified code
    Load the environmental layers from the downloaded_predictors folder and extract the environmental information into the md file

--- Step2_Get_environmental_layers.R
+++ Step2_Get_environmental_layers.R
@@ -1,19 +1,12 @@
 ###
 
-#Step 2 obtain environmental covariates
-
-#Code to obtain environmental covariates using MARSPEC and COPERNICUS repositories
-
+#Step 2 Load environmental covariates
 #Tania Valdivia Carrillo
 
-###
+#Environmental covariates were obtained from MARSPEC and COPERNICUS repositories
 
 #Install packages
-
-list.of.packages=c("sdmpredictors","leaflet", "recipes","raster", "ncdf4","ggplot2","viridis","RColorBrewer",
-                   "ggsn","rnaturalearth","rnaturalearthdata","kableExtra","biomod2","knitr","readr",
-                   "magrittr","taxize","stars","dplyr","sf","scico","patchwork","stringr",
-                   "stringi","vegan","tidyr", "librarian","sp","plyr","rgdal","mgcv","gratia")
+list.of.packages=c("raster", "rgdal", "dplyr")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies = T)
@@ -26,65 +19,16 @@ source('custom_functions.R')
 #remotes::install_github("michaeldorman/geobgu", force = TRUE)
 library("geobgu")
 ###
-#setwd("~/Library/CloudStorage/GoogleDrive-tania.valdiviac@gmail.com/My Drive/2.2023/05_MURI_Module_3_Tania/Documents/Manuscript_eDNA-occurrenceSDM/Github/")
--------------------------------------------
-  #Retrieve Environmental Data, 
-  #this is provitional environmental data not from the sampling dates, but means from 1994 to 2009
-------------------------------------------
+setwd("~/Library/CloudStorage/GoogleDrive-tania.valdiviac@gmail.com/My Drive/2.2023/05_MURI_Module_3_Tania/Documents/Manuscript_eDNA-occurrenceSDM/Github/")
 
-# Extent of area of interest California Current
-# latmax <- 48.5416666716337204
-# latmin <-37.7916653901338577
-# lonmax <- -122.5416692097981866
-# lonmin <- -126.6250025431315152
+####Load predictors from downloaded_predictors folder. 
+# Surface data, resolution 0.083x0.083, date: 08/2019
 
-# Extent of area of interest WA cost
-latmax <- 50.0416
-latmin <- 32.375
-lonmax <- -115.7917
-lonmin <- -130.2083
-
-# Create a spatial bounding box for the area of interest using the 'sf' package:
-# create a matrix:
-bb <- cbind(c(lonmin,lonmax,lonmax,lonmin,lonmin),
-            c(latmin,latmin,latmax,latmax,latmin)) %>%
-  # put that matrix into a list, because that's what `st_polygon()` needs
-  list() %>%
-  # Make the matrix a 'simple features' polygon:
-  sf::st_polygon() %>%
-  # and let's make it a simple feature column and give it information about the projection:
-  sf::st_sfc(crs="+proj=longlat +datum=WGS84") %>%
-  # finally, let's put the sfc in a simple features data.frame in the variable `geometry`:
-  sf::st_sf(name="Study Site",geometry=.)
-
---------------------------------------
-  # Download environmental layers
-  --------------------------------------
-  
-#Select envirnnomental variables. More infor see https://bio-oracle.org/code.php, http://marspec.org/
-datasets <- list_datasets(terrestrial = FALSE, marine = TRUE)
-layers_all <- list_layers(datasets)
-#write.csv(layers, "Environmental_layers_metadata.csv")
-
-# Create directory and the list of environmental variables you need
-#dir.create("downloaded_predictors/Environmental_Seascape/", showWarnings = FALSE)
-#Which variables?
-
-#Static variables
-layers <- c("MS_bathy_5m","MS_biogeo05_dist_shore_5m","MS_biogeo06_bathy_slope_5m")
-
-# Download marine layers to directory. 
-
-sdmpredictors::load_layers(layers, datadir = "./downloaded_predictors/", rasterstack = TRUE)
-
-####envCov_USA_surface_083_082019
-
-bathy_083<-raster('./downloaded_predictors/MS_bathy_5m_lonlat.tif')
-dist_shore_083<-raster('./downloaded_predictors/MS_biogeo05_dist_shore_5m_lonlat.tif')
-slope_083<-raster('./downloaded_predictors/MS_biogeo06_bathy_slope_5m_lonlat.tif')
-
-#Manually obtain from env layers from COPERNICUS and load them
-#surface_083_082019
+#BioOracle
+bathy_083<-raster('./downloaded_predictors/bathy.tif')
+dist_shore_083<-raster('./downloaded_predictors/dist_shore.tif')
+slope_083<-raster('./downloaded_predictors/slope.tif')
+#COPERNICUS LAYERS
 ESV_surface_083_082019<-raster('./downloaded_predictors/ESV.tif')
 epi_surface_083_022019<-raster('./downloaded_predictors/epi.tif')
 lmeso_surface_083_022019<-raster('./downloaded_predictors/lmeso.tif')
@@ -147,7 +91,6 @@ writeRaster(envCov_spol_surface_083_082019,"./downloaded_predictors/envCov_spol_
 
 envCov_spol_surface_083_082019_df <- as.data.frame(envCov_spol_surface_083_082019, xy = TRUE)
 
-
 (p <- ggplot() +
     geom_raster(data = envCov_spol_surface_083_082019_df, aes(x = x, y = y, fill = lat)) +
     scale_fill_viridis_c() +
@@ -166,16 +109,12 @@ envCov_spol_surface_083_082019_df <- as.data.frame(envCov_spol_surface_083_08201
 #coordinates(coord) <-c("lon", "lat")
 md.taxa.long.formated<-read.csv("./dataframes/md_taxa_long_formated.csv") #ordered by latitude 
 coord <- md.taxa.long.formated[,c(24,23)] # Order must be lon lat
-# coord.dec<-SpatialPoints(coord, proj4string=CRS("+proj=longlat"))
-# coord.dec.df<-as.data.frame(coord)
-# coords <- st_as_sf(coord, coords = c("lon", "lat"))
-sites.names<-md.taxa.long.formated$sampleID_station
+envCov_surface_083_df<-raster::extract(envCov_spol_surface_083_082019,coord,df=TRUE) %>% 
+  select(!c(lat,lon))
 
-envCov_surface_083_df<-raster::extract(envCov_spol_surface_083_082019,coord,df=TRUE)
-
-md.taxa.long.formated.env083<-bind_cols(md.taxa.long.formated,envCov_surface_083_df)
-
-write.csv(envCov_surface_083_df, "./dataframes/envCov_surface_083_df.csv")
-write.csv(md.taxa.long.formated.env083, "./dataframes/md_taxa_long_formated_env083.csv")
+md.taxa.long.formated.env083<-bind_cols(md.taxa.long.formated,envCov_surface_083_df) 
+  
+#write.csv(envCov_surface_083_df, "./dataframes/envCov_surface_083_df.csv")
+#write.csv(md.taxa.long.formated.env083, "./dataframes/md_taxa_long_formated_env083.csv")
 
 
